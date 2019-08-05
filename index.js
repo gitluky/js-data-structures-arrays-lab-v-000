ac6b09b709dc9447525a1cbee6c6b3d10fe1e32e
Jonathan Luk <jluk87@gmail.com>
2019-08-05 21:41:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ function removeLastDriver () {
 }
 
 function removeFirstDriver () {
+<<<<<<< HEAD
   const new_drivers_array = drivers.slice(1);
+=======
+  const new_drivers_array = drivers.splice(0,1);
+>>>>>>> 3023f31267b299cc41c769c3820511a0c5fb8d51
   return new_drivers_array;
 }
